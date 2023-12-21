ec2f63045fcb341fb64248968d713979c41426d1
Vagner Kuntz <vaagner.kuntz@gmail.com>
2023-12-21 19:59:57 -0300

    adicionado tema com styled-components

---
+++ src/screens/SignIn/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from 'react-native';
+import { styles } from './styles';
+
+export function SignIn() {
+  return (
+    <View style={styles.container}>
+      <Text>SignIn</Text>
+    </View>
+  );
+}
---
+++ src/theme/index.ts
@@ -0,0 +1,17 @@
+export default {
+  colors: {
+    white: '#FFFFFF',
+    brandLight: '#00649A',
+    brandMid: '#005A97',
+    brandDark: '#004C89'
+  },
+  fontFamily: {
+    regular: 'OpenSans_400Regular',
+    bold: 'OpenSans_700Bold'
+  },
+  fontSize: {
+    small: 14,
+    medium: 16,
+    large: 18
+  }
+} as const
