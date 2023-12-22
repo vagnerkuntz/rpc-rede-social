4f72588beeac62fc087a65183820033ec1ea33a2
Vagner Kuntz <vaagner.kuntz@gmail.com>
2023-12-22 09:45:02 -0300

    implementacao de fontes personalizadas

--- App.tsx
+++ App.tsx
@@ -1,8 +1,20 @@
 import { ThemeProvider } from 'styled-components/native'
-import { SignIn } from './src/screens/SignIn';
-import theme from './src/theme';
+import { useFonts, OpenSans_400Regular, OpenSans_700Bold } from '@expo-google-fonts/open-sans'
+
+import { SignIn } from './src/screens/SignIn'
+import theme from './src/theme'
+import { Loading } from './src/components/Loading'
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    OpenSans_400Regular,
+    OpenSans_700Bold
+  })
+
+  if (!fontsLoaded) {
+    return <Loading />
+  }
+
   return (
     <ThemeProvider theme={theme}>
       <SignIn />
---
+++ src/components/Loading/styles.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components/native";
+import theme from "../../theme";
+
+export const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  background-color: ${theme.colors.white};
+`
+
+export const LoadIndicator = styled.ActivityIndicator.attrs(() => ({
+  size: "large",
+  color: theme.colors.brandDark
+}))``;
